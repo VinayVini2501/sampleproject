214dc7a2dd4ef6dc16efd304522788483c421dd9
Vinay V <Vinay V@DESKTOP-DRH83U0>
2023-01-23 11:42:57 +0530

    1commit

---
+++ src/test/java/simpletest/Delete.java
@@ -0,0 +1,14 @@
+package simpletest;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Delete
+{
+	public void delete()
+	{
+		
+		System.out.println("deleted");
+	}
+
+}
---
+++ src/test/java/simpletest/Modify.java
@@ -0,0 +1,14 @@
+package simpletest;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Modify 
+{
+	public void modify()
+	{
+		
+		System.out.println("modified");
+	}
+}
+
